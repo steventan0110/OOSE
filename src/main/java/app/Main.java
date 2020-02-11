02eb866fe5b5958fce4b21cfff5cf712c2235f58
Steven Tan <wtan12@jhu.edu>
2020-02-10 21:10:06 -0500

    update project structure with login page and user controller

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,12 +1,13 @@
+package app;
 import io.javalin.Javalin;
 import io.javalin.http.staticfiles.*;
 import static io.javalin.apibuilder.ApiBuilder.*;
 
-public class HelloWorld {
+public class Main {
     public static void main(String[] args) {
-//        Javalin app = Javalin.create().start(7000);
         Javalin app = Javalin.create(config -> { config.addStaticFiles("/public"); });
         app.start(8080);
         //app.get("/", ctx -> ctx.result("Hello World"));
+        app.error(404, ctx -> ctx.result("Your requested Page is not found!"));
     }
 }
